16d75b56477926a6eb22056cabc3861c4c091423
prithvikr66 <pkunofficial66@gmail.com>
2024-12-11 16:17:23 +0530

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,18 @@ import { ThirdwebProvider, ChainId } from "@thirdweb-dev/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
 import { SolanaWalletProvider } from "@/hooks/wallet-provider";
-
+import { Suspense } from "react";
 export default function Home() {
   const [queryClient] = useState(() => new QueryClient());
   return (
     <QueryClientProvider client={queryClient}>
       <SolanaWalletProvider>
         <ThirdwebProvider activeChain={ChainId.Mainnet}>
-          <div className=" h-screen w-full flex items-center justify-center">
-            <CryptoForm />
-          </div>
+          <Suspense fallback={<div>Loading...</div>}>
+            <div className=" h-screen w-full flex items-center justify-center">
+              <CryptoForm />
+            </div>
+          </Suspense>
         </ThirdwebProvider>
       </SolanaWalletProvider>
     </QueryClientProvider>
